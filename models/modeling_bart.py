de0411b0f31b26336d08ec22d48a678dbea8e164
roy <rsy0702@163.com>
2020-10-18 16:35:56 +0800

    add modeling files

---
+++ models/modeling_bart.py
@@ -0,0 +1,9 @@
+'''
+Author: your name
+Date: 2020-10-18 15:41:56
+LastEditTime: 2020-10-18 15:57:51
+LastEditors: Please set LastEditors
+Description: In User Settings Edit
+FilePath: /generation/models/modeling_bart.py
+'''
+from transformers import BartModel
